36079d94c1ccf55cc0ab1e011e3ec38bd985611a
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-03-15 14:10:32 +0300

    Check and Create Wallet Address HD  and Private
    Check and Create Wallet Address HD  and Private Key High Speed

---
+++ ethxbnb.py
@@ -0,0 +1,163 @@
+mmdrza = '''
+
+=========================[M M D Z A . C o M]==============================
+             _ __ ___  _ __ ___   __| |_ __ ______ _
+            | '_ ` _ \| '_ ` _ \ / _` | '__|_  / _` |
+            | | | | | | | | | | | (_| | |   / / (_| |
+            |_| |_| |_|_| |_| |_|\__,_|_|  /___\__,_|
+=========================[M M D Z A . C o M]==============================
+= Athur : MMDRZA
+= Email : x4@Mmdrza.Com
+= Web : https://Mmdrza.Com
+= Dev.to/mmdrza
+= Github.Com/Pymmdrza
+=========================[M M D Z A . C o M]==============================
+= Donat = Bitcoin 16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8
+=========================[M M D Z A . C o M]==============================
+        '''
+
+from hdwallet import HDWallet
+from hdwallet.symbols import ETH as SYMBOL
+import random
+import requests
+import json
+from colorama import Fore, Style
+import time
+
+# =========================[M M D Z A . C o M]==============================
+print(Fore.GREEN,mmdrza,Style.RESET_ALL)
+print('Start ...')
+time.sleep(5)
+# =========================[M M D Z A . C o M]==============================
+z = 1
+while True:
+    c1 = str(random.choice('0123456789abcdefABCDEF'))
+    c2 = str(random.choice('0123456789abcdefABCDEF'))
+    c3 = str(random.choice('0123456789abcdefABCDEF'))
+    c4 = str(random.choice('0123456789abcdefABCDEF'))
+    c5 = str(random.choice('0123456789abcdefABCDEF'))
+    c6 = str(random.choice('0123456789abcdefABCDEF'))
+    c7 = str(random.choice('0123456789abcdefABCDEF'))
+    c8 = str(random.choice('0123456789abcdefABCDEF'))
+    c9 = str(random.choice('0123456789abcdefABCDEF'))
+    c10 = str(random.choice('0123456789abcdefABCDEF'))
+    c11 = str(random.choice('0123456789abcdefABCDEF'))
+    c12 = str(random.choice('0123456789abcdefABCDEF'))
+    c13 = str(random.choice('0123456789abcdefABCDEF'))
+    c14 = str(random.choice('0123456789abcdefABCDEF'))
+    c15 = str(random.choice('0123456789abcdefABCDEF'))
+    c16 = str(random.choice('0123456789abcdefABCDEF'))
+    c17 = str(random.choice('0123456789abcdefABCDEF'))
+    c18 = str(random.choice('0123456789abcdefABCDEF'))
+    c19 = str(random.choice('0123456789abcdefABCDEF'))
+    c20 = str(random.choice('0123456789abcdefABCDEF'))
+    c21 = str(random.choice('0123456789abcdefABCDEF'))
+    c22 = str(random.choice('0123456789abcdefABCDEF'))
+    c23 = str(random.choice('0123456789abcdefABCDEF'))
+    c24 = str(random.choice('0123456789abcdefABCDEF'))
+    c25 = str(random.choice('0123456789abcdefABCDEF'))
+    c26 = str(random.choice('0123456789abcdefABCDEF'))
+    c27 = str(random.choice('0123456789abcdefABCDEF'))
+    c28 = str(random.choice('0123456789abcdefABCDEF'))
+    c29 = str(random.choice('0123456789abcdefABCDEF'))
+    c30 = str(random.choice('0123456789abcdefABCDEF'))
+    c31 = str(random.choice('0123456789abcdefABCDEF'))
+    c32 = str(random.choice('0123456789abcdefABCDEF'))
+    c33 = str(random.choice('0123456789abcdefABCDEF'))
+    c34 = str(random.choice('0123456789abcdefABCDEF'))
+    c35 = str(random.choice('0123456789abcdefABCDEF'))
+    c36 = str(random.choice('0123456789abcdefABCDEF'))
+    c37 = str(random.choice('0123456789abcdefABCDEF'))
+    c38 = str(random.choice('0123456789abcdefABCDEF'))
+    c39 = str(random.choice('0123456789abcdefABCDEF'))
+    c40 = str(random.choice('0123456789abcdefABCDEF'))
+    c41 = str(random.choice('0123456789abcdefABCDEF'))
+    c42 = str(random.choice('0123456789abcdefABCDEF'))
+    c43 = str(random.choice('0123456789abcdefABCDEF'))
+    c44 = str(random.choice('0123456789abcdefABCDEF'))
+    c45 = str(random.choice('0123456789abcdefABCDEF'))
+    c46 = str(random.choice('0123456789abcdefABCDEF'))
+    c47 = str(random.choice('0123456789abcdefABCDEF'))
+    c48 = str(random.choice('0123456789abcdefABCDEF'))
+    c49 = str(random.choice('0123456789abcdefABCDEF'))
+    c50 = str(random.choice('0123456789abcdefABCDEF'))
+    c51 = str(random.choice('0123456789abcdefABCDEF'))
+    c52 = str(random.choice('0123456789abcdefABCDEF'))
+    c53 = str(random.choice('0123456789abcdefABCDEF'))
+    c54 = str(random.choice('0123456789abcdefABCDEF'))
+    c55 = str(random.choice('0123456789abcdefABCDEF'))
+    c56 = str(random.choice('0123456789abcdefABCDEF'))
+    c57 = str(random.choice('0123456789abcdefABCDEF'))
+    c58 = str(random.choice('0123456789abcdefABCDEF'))
+    c59 = str(random.choice('0123456789abcdefABCDEF'))
+    c60 = str(random.choice('0123456789abcdefABCDEF'))
+    c61 = str(random.choice('0123456789abcdefABCDEF'))
+    c62 = str(random.choice('0123456789abcdefABCDEF'))
+    c63 = str(random.choice('0123456789abcdefABCDEF'))
+    c64 = str(random.choice('0123456789abcdefABCDEF'))
+    magic = (c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12 + c13 + c14 + c15 + c16 + c17 + c18 + c19 + c20 + c21 + c22 + c23 + c24 + c25 + c26 + c27 + c28 + c29 + c30 + c31 + c32 + c33 + c34 + c35 + c36 + c37 + c38 + c39 + c40 + c41 + c42 + c43 + c44 + c45 + c46 + c47 + c48 + c49 + c50 + c51 + c52 + c53 + c54 + c55 + c56 + c57 + c58 + c59 + c60 + c61 + c62 + c63 + c64)
+    # Start API
+    # =========================[M M D Z A . C o M]==============================
+    api1 = "&apikey=ENTER-YOUR-API-EtherScan.iO"
+    api2 = "&apikey=ENTER-YOUR-API-EtherScan.iO"
+    api3 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api4 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api5 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api6 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api7 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api8 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api9 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api10 = "&apiKey=ENTER-YOUR-API-EtherScan.iO"
+    api11 = "&apiKey=UBUQMB3NBU8RU89D3SAREY9Q92BV31IC56"
+    api12 = "&apiKey=FH5NC8S4XZH6TU4E2VE8FH9FEBVW1VF61N"
+    # =========================[M M D Z A . C o M]==============================
+    bapi1 = "&apikey=ENTER-YOUR-API-BSCSCAN.iO"
+    bapi2 = "&apikey=ENTER-YOUR-API-BSCSCAN.iO"
+    bapi3 = "&apiKey=ENTER-YOUR-API-BSCSCAN.iO"
+    bapi4 = "&apiKey=ENTER-YOUR-API-BSCSCAN.iO"
+    bapi5 = "&apiKey=ENTER-YOUR-API-BSCSCAN.iO"
+    bapi6 = "&apiKey=ENTER-YOUR-API-BSCSCAN.iO"
+    mylist1 = [str(bapi1), str(bapi2), str(bapi3), str(bapi4), str(bapi5), str(bapi6)]
+    apikeysbnb = random.choice(mylist1)
+    # =======================[M M D Z A . C o M]================================
+    mylist = [str(api1), str(api2), str(api3), str(api4), str(api10), str(api11), str(api12),str(api5), str(api6), str(api7), str(api8), str(api9)]
+    # =========================[M M D Z A . C o M]==============================
+    apikeys = random.choice(mylist)
+    # =========================[M M D Z A . C o M]==============================
+    PRIVATE_KEY = str(magic)
+    hdwallet: HDWallet = HDWallet(symbol=SYMBOL)
+    hdwallet.from_private_key(private_key=PRIVATE_KEY)
+    privatekey = hdwallet.private_key()
+    ethaddr = hdwallet.p2pkh_address()  # 0xEF3589b72bd2f2e539843aa0Af67FEd18906A761
+    blocs = requests.get("https://api.etherscan.io/api?module=account&action=balance&address=" + ethaddr + apikeys)
+    ress = blocs.json()
+    baleth = dict(ress)["result"]
+    blocs1 = requests.get("https://api.bscscan.com/api?module=account&action=balance&address=" + ethaddr + apikeysbnb)
+    ress1 = blocs1.json()
+    balbnb = dict(ress1)["result"]
+    # =========================[M M D Z A . C o M]==============================
+    print(str(z),Fore.YELLOW,'ADDR =',Fore.WHITE,str(ethaddr),Fore.YELLOW,'PRIV =',Fore.GREEN,str(privatekey),Fore.RED,'[ ETH=',str(baleth),'BNB=',str(balbnb),']')
+    z += 1
+    # =========================[M M D Z A . C o M]==============================
+    if int(balbnb) or int(baleth) > 0:
+        print('Win Wallet --------- Save On Text File --------')
+        print('----------------------[M M D Z A . C o M]----------------------')
+        print('Address =>', ethaddr)
+        print('PrivateKey =>', privatekey)
+        f = open("ETHxBNB.txt","a")
+        f.write('\nAddress = ' + ethaddr)
+        f.write('\nPrivate = ' + privatekey)
+        f.write('\n===================================[M M D Z A . C o M]===================================')
+        f.close()
+        continue
+        
+        
+        
+        
+# =========================[M M D Z A . C o M]==============================
+# Programmer M M D R Z A 
+# Web Mmdrza.Com
+# Dev.to/Mmdrza
+# Github.Com/Pymmdrza
+# Donat = Bitcoin 16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8
+# =========================[M M D Z A . C o M]==============================
